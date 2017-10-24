07eb4b1488a40be79fb9d1424a7385123bf7ad85
Colin Ashburn <cashburn@purdue.edu>
2017-10-24 17:31:59 -0400

    Allow "0 pizzas" and change TestCase name

--- Practice18/Project.java
+++ Practice18/Project.java
@@ -18,7 +18,7 @@ public class Project {
 		try {
 			TestCase[] tests = {
 				new TestCase("Test 1", "File compiles", "Completion", 1),
-				new TestCase("Test 2", "switch statement is created", "Completion", 1),
+				new TestCase("Test 2", "Ternary statement is written", "Completion", 1),
 				new TestCase("Test 3", "output is correct", "Completion", 1)
 			};
 
@@ -26,7 +26,7 @@ public class Project {
 				System.out.println("Practice.java does not compile.");
 			} else {
 				tests[0].setResult(true);
-				tests[2].setResult(TestCase.runMain(".", "Practice", null, "(?s).*I have 1 pizza\\..*I have 10 pizzas.*I have 0 pizza\\..*"));
+				tests[2].setResult(TestCase.runMain(".", "Practice", null, "(?s).*I have 1 pizza\\..*I have 10 pizzas.*I have 0 pizzas?\\..*"));
 			}
 
 			Parser parser = new Parser();
