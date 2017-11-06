dfc374688df23337ec4f4dfc3045a99e49c39613
Robyn Berkel <rberkel@purdue.edu>
2017-11-06 11:33:32 -0500

    prime numbers fix

--- Alphabet/Alphabet.java
+++ Alphabet/Alphabet.java
@@ -1,22 +1,33 @@
 import java.util.Scanner;
-
 public class Alphabet {
 
-	public static String whatsMissing(String str) {
-		str = str.toLowerCase();
-		String result = "";
-		for (int i = 'a'; i <= 'z'; i++) {
-			if (str.indexOf(i) == -1) {
-				result += (char)i;
-			}
-		}
-		return result;
-	}
+    public static String popFrom(String poppee, String popper) {
+        if (poppee.contains(popper)) {
+            int toRemove = poppee.indexOf(popper);
+            return poppee.substring(0, toRemove) + poppee.substring(toRemove+1);
+        } else {
+            return poppee;
+        }
+    }
 
-	public static void main(String[] args) {
-		Scanner read = new Scanner(System.in);
-		String line = read.nextLine();
-		System.out.println("The missing letters are: " + whatsMissing(line));
-	}
+    public static String whatsMissing (String sentence) {
+        String ans = "abcdefghijklmnopqrstuvwxyz";
+        String mySentence = sentence.toLowerCase();
+        for (int i = 0; i < mySentence.length(); i++) {
+            String myString = mySentence.charAt(i) + "";
+            if (ans.contains(myString)) {
+                ans = popFrom(ans, myString);
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please type in a sentence, then I will print all the letters that aren't in the sentence.");
+        String myWords = s.nextLine();
+        String printMe = "The missing letters are: %s";
+        System.out.printf(printMe, whatsMissing(myWords));
+    }
 
 }
