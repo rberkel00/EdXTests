b8eba0a14b745d7ffc8de51a7b8efc6121637d57
tkotekar <tejaswi.kotekar@gmail.com>
2018-02-13 06:13:07 -0500

    Week 5 Practices

---
+++ Practice-InheritanceBasics/solution/Practice.java
@@ -0,0 +1,34 @@
+ class BaseClass {
+     protected int x;
+     protected int y;
+
+     public BaseClass(int x, int y) {
+          this.x = x;
+          this.y = y;
+     }
+
+     public String toString() {return "x: " + x + "; y: " + y;}
+}
+
+/*** TODO: Write the header for the ChildClass class, which inherits from BaseClass ***/
+ class ChildClass extends BaseClass
+{
+     /*** TODO: Write the header for the ChildClass constructor, taking two inputs, x and y ***/
+      ChildClass(int x, int y)
+     {
+          super(x,y);
+          this.x = 2 * x;
+          this.y = 2 * y;
+     }
+}
+
+public class Practice {
+
+     public static void main(String[] args) {
+          /*** TODO: Create a new ChildClass object with inputs x = 4, y = 3, and store it in a
+                     ChildClass reference, c ***/
+                     ChildClass c = new ChildClass(4, 3);
+
+          System.out.println(c);
+     }
+}
---
+++ Practice-ObjectClass/solution/Practice.java
@@ -0,0 +1,27 @@
+/*** TODO: Write a header for the Pizza class which inherits from Object ***/
+class Pizza extends Object
+{
+     private String[] toppings;
+
+     public Pizza(String a, String b, String c) {
+          toppings = new String[3];
+          toppings[0] = a;
+          toppings[1] = b;
+          toppings[2] = c;
+     }
+
+     /*** TODO: Write the toString method header to override the Object class toString method ***/
+     public String toString()
+     {
+          String s = "Pizza with ";
+          for (String t : toppings) {s = s + t + " ";}
+          return s;
+     }
+}
+
+public class Practice {
+     public static void main(String[] args) {
+          Pizza p = new Pizza("Pepperoni","Mushrooms","Sausage");
+          System.out.println(p);
+     }
+}
