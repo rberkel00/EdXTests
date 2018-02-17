59978a286fcbdc0835b3aca431a616bc15b484d9
tkotekar <tejaswi.kotekar@gmail.com>
2018-02-17 17:02:10 -0500

    week 6

---
+++ Practice-AccessModifiers/Project.java
@@ -0,0 +1,64 @@
+import com.github.javaparser.*;
+import com.github.javaparser.ast.*;
+import com.github.javaparser.ast.comments.*;
+import com.github.javaparser.ast.body.*;
+import java.io.*;
+import java.util.*;
+import java.nio.file.Files;
+import java.nio.charset.StandardCharsets;
+import com.github.javaparser.ast.expr.*;
+import com.github.javaparser.metamodel.*;
+import com.github.javaparser.ast.stmt.*;
+import com.github.javaparser.ast.type.*;
+public class Project {
+	public static void main(String[] args) {
+
+		//define tests
+		try {
+			TestCase[] tests = {
+				new TestCase("Test 1", "File compiles", "Completion", 1),
+				new TestCase("Test 2", "Correct field declarations", "Completion", 1),
+				new TestCase("Test 3", "Correct accessor method header", "Completion", 1),
+				new TestCase("Test 4", "Correct output", "Completion", 1)
+			};
+
+			if (!TestCase.compile(new File("Practice.java"))) {
+				System.out.println("Practice.java does not compile.");
+			} else {
+				tests[0].setResult(true);
+				tests[3].setResult(TestCase.runMain(".", "Practice", null, "(?s).*1.*"));
+			}
+
+			Parser lpar = new Parser();
+			if (lpar.parse("Practice.java")) {
+				for (MethodDeclaration m : lpar.methods) {
+					if (m.getDeclarationAsString(true, false, false).equals("private int getA()")) {
+						tests[2].setResult(true);
+					}
+				}
+				boolean[] vars = new boolean[3];
+				for (FieldDeclaration f : lpar.fields) {
+					for (Modifier mo : f.getModifiers()) {
+						if (mo.asString().equals("private")) {
+							for (VariableDeclarator v : f.getVariables()) {
+								if (v.getName().asString().equals("a")) {
+									vars[0] = true;
+								} else if (v.getName().asString().equals("b")) {
+									vars[1] = true;
+								} else if (v.getName().asString().equals("c")) {
+									vars[2] = true;
+								}
+							}
+						}
+					}
+				}
+				if (vars[0] && vars[1] && vars[2]) tests[1].setResult(true);
+			}
+
+ 			TestCase.pushAll(tests);
+
+		} catch (Exception e) {
+		}
+
+}
+}
---
+++ Practice-OverrideMethods/A.java
@@ -0,0 +1,13 @@
+public class A
+{
+     private int a;
+
+     public A(int a) {
+          this.a = a;
+     }
+
+     public int doSomething(int b)
+     {
+          return a * b;
+     }
+}
