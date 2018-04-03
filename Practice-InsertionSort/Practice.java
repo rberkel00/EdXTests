52ea456221e3abf3ad3ae172a09bb56d209efc48
Robyn Berkel <rberkel@purdue.edu>
2018-04-02 22:22:44 -0400

    insertion practice

--- Practice-InsertionSort/Practice.java
+++ Practice-InsertionSort/Practice.java
@@ -10,10 +10,13 @@ public class Practice {
           for (int x = 1; x < data.size(); x++) {
                /*** TODO: Write the inner loop that walks from the current value of the
                           outer for-loop until that value is greater than the value in
-                          the position one less than it ***/
-               {
+                          the position one less than it or it reaches the beginning of the list***/
+               for (int y = x; y > 0 && data.get(y).compareTo(data.get(y-1)) == -1; y--) {
                    /*** TODO: Swap the value referenced by the inner loop with the value in
                               the position one less than it ***/
+                              temp = data.get(y);
+                              data.set(y, data.get(y-1));
+                              data.set(y-1, temp);
                }
           }
 
@@ -24,7 +27,7 @@ public class Practice {
      public static void main(String[] args) {
           Random rnd = new Random(2018);
           ArrayList<Comparable> myList = new ArrayList<Comparable>();
-          long timeTaken;
+          long timeTaken = 0;
 
           for (int i = 0; i < 20; i++) {
                myList.add(rnd.nextInt(100));
@@ -34,8 +37,9 @@ public class Practice {
           try {
                /*** TODO: Write a function call to the insertion method, passing myList
                           as a parameter and storing the result in timeTaken ***/
+                          insertion(myList);
           } catch (NullPointerException e) {
-               System.err.println(e.printStackTrace());
+               e.printStackTrace();
           }
 
           System.out.println(myList);
--- Practice-InsertionSort/Project.java
+++ Practice-InsertionSort/Project.java
@@ -17,18 +17,38 @@ public class Project {
 		try {
 			TestCase[] tests = {
 				new TestCase("Test 1", "File compiles", "Completion", 1),
-				new TestCase("Test 2", "Correct array list output", "Completion", 1),
-				new TestCase("Test 3", "Correct array list size", "Completion", 1)
+				new TestCase("Test 2", "One other loop is created", "Completion", 1),
+				new TestCase("Test 3", "Funciton call is made in main", "Completion", 1),
+				new TestCase("Test 4", "Output is correct", "Completion", 1)
 			};
 
 			if (!TestCase.compile(new File("Practice.java"))) {
 				System.out.println("Practice.java does not compile.");
 			} else {
 				tests[0].setResult(true);
-				tests[1].setResult(TestCase.runMain(".", "Practice", null, "(?s).*LaKisha\nMichael\nGrace\nMichael.*"));
-				tests[2].setResult(TestCase.runMain(".", "Practice", null, "(?s).*4.*"));
+				tests[3].setResult(TestCase.runMain(".", "Practice", null, "(?s).*0, 10, 10, 10, 15, 21, 33, 33, 34, 43, 44, 50, 55, 67, 72, 73, 78, 87, 95, 96.*"));
 			}
 
+			Parser p = new Parser();
+			if (p.parse("Practice.java")) {
+				List<Node> mnodes = p.findPieces("void main(String[])");
+				List<Node> snodes = p.findPieces("long insertion(ArrayList<Comparable>)");
+				for (Node n : mnodes) {
+					if (n instanceof MethodCallExpr) {
+						MethodCallExpr m = (MethodCallExpr)n;
+						if (m.getName().asString().equals("insertion")) {
+							tests[2].setResult(true);
+						}
+					}
+				}
+				int count = 0;
+				for (Node n : snodes) {
+					if (n instanceof ForeachStmt || n instanceof ForStmt || n instanceof WhileStmt || n instanceof DoStmt) {
+						count++;
+					}
+				}
+				if (count > 1) tests[1].setResult(true);
+			}
 
  			TestCase.pushAll(tests);
 
